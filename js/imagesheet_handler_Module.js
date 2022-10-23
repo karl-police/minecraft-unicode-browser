0a02710c337dcaba7dc63e6b3f5657346a80f474
karl-police <karl-police2001@bluewin.ch>
2022-10-23 21:10:46 +0200

    Update imagesheet_handler_Module.js

--- js/imagesheet_handler_Module.js
+++ js/imagesheet_handler_Module.js
@@ -40,8 +40,86 @@ function cropImage(imgURI, width, height, posX, posY) {
 }
 
 
+// A function to generate separate images from an image sheet.
+function regularImageSheet_extract(options) {
+    return new Promise(function(resolve, reject) {
+        if (!options) {
+            options = {}
+        }
+    
+        sheetImgSrc = options.sheetImgSrc
+        sheetWidth = options.sheetWidth
+        sheetHeight = options.sheetHeight
+    
+        cropWidth = options.cropWidth
+        cropHeight = options.cropHeight
+
+        offsetX = options.offsetX
+        offsetY = options.offsetY
+
+        rowAmount = options.rowAmount
+        columnAmount = options.columnAmount
+
+        charToStopAt = options.charToStopAt
+
+
+        let totalPosCounter = 1;
+        let rowPosCounter = 1;
+        let columnPosCounter = 1;
+
+        let collectionTable = {};
+        collectionTable.images = {};
+
+        /* canvas */
+        var transform_canvas = document.createElement("canvas")
+        var img_src = new Image()
+
+        img_src.src = sheetImgSrc;
+        
+        if (__DEBUG) {
+            img_src.crossOrigin = "anonymous"
+        }
+
+        img_src.onload = function() {
+            transform_canvas.width = cropWidth
+            transform_canvas.height = cropHeight
+
+            var canvas2D = transform_canvas.getContext("2d")
+
+            for (col_i=1; col_i <= columnAmount; col_i++) {
+                for (row_i=1; row_i <= rowAmount; row_i++) {
+                    canvas2D.drawImage(img_src, ((rowPosCounter - 1) * offsetX), ((columnPosCounter - 1) * offsetY), cropWidth, cropHeight, 0, 0, cropWidth, cropHeight)
+                    
+                    var dataURL = transform_canvas.toDataURL("image/png").toString()
+
+                    Object.assign(collectionTable.images, {[totalPosCounter]: dataURL})
+    
+                    // break if charToStopAt was reached
+                    if (totalPosCounter == charToStopAt) {
+                        break
+                    }
+                    
+                    canvas2D.clearRect(0, 0, transform_canvas.width, transform_canvas.height)
+
+                    rowPosCounter += 1;
+                    totalPosCounter += 1;
+                }
+
+                columnPosCounter += 1;
+                rowPosCounter = 1; // Reset
+            }
+
+            if (collectionTable) {
+                resolve(collectionTable)
+            }
+        }
+    })
+}
+
+
 // export for module
 this.cropImage = cropImage
+this.regularImageSheet_extract = regularImageSheet_extract
 
 
 } // end of module
