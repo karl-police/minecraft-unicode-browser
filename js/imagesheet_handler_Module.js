20639082d3df77e2de33d106c64669321fc78c45
karl-police <karl-police2001@bluewin.ch>
2022-10-23 15:10:15 +0200

    Update imagesheet_handler_Module.js

--- js/imagesheet_handler_Module.js
+++ js/imagesheet_handler_Module.js
@@ -3,10 +3,35 @@ const imagesheet_handler_Module = new function() {
 //
 
 
+function test_cropImg(imgURI, width, height, posX, posY) {
+    return new Promise(function(resolve, rejected) {
+        var transform_canvas = document.createElement("canvas")
+        var img_src = new Image()
+
+        img_src.src = imgURI;
+
+        img_src.onload = function() {
+            transform_canvas.width = width
+            transform_canvas.height = height
+
+            var canvas2D = transform_canvas.getContext("2d")
+            canvas2D.drawImage(img_src, posX, posY, width, height, 0, 0, width, height)
+
+            var result = transform_canvas.toDataURL("image/png").toString()
+
+            if (result) {
+                console.log(result)
+                resolve(result)
+            }
+        }
+        
+        document.getElementById("test_2").append(transform_canvas)
+    })
+}
 
 
-
-
+// export for module
+this.test_cropImg = test_cropImg
 
 
 } // end of module
